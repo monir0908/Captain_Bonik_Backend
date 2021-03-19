951aa5a315b066eb087f66951ee4f3eb62d76d7d
Monir Hossain <monirhossain0908@gmail.com>
2021-03-19 22:19:12 +0600

    Project Initialized

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+
+@app.get('/test')
+def test_view():
+    return {
+        "IsReport":"Success",
+        "Msg": "It is working"
+    }
+
+@app.get('/')
+def doc_view():
+    return RedirectResponse(url="/docs/")
